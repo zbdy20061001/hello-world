43c15ab3bb7c36d9587e15f1f389954054e67d62
Ben Zhang <zbdy20061001@163.com>
2019-07-29 22:25:33 +0800

    some update

--- java-samples/src/main/java/jvm/TestClass.java
+++ java-samples/src/main/java/jvm/TestClass.java
@@ -1,9 +1,16 @@
 package jvm;
 
-public class TestClass {
+public class TestClass implements Cloneable{
 	private int m;
 
 	public int inc() {
 		return m + 1;
 	}
+	
+	@Override
+    protected Object clone() throws CloneNotSupportedException {
+
+        return super.clone();
+
+    }
 }
