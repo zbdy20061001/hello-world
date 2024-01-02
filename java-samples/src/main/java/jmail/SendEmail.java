9e27ea697953142dea6b4c8068de1d8683e8b3e2
Ben Zhang <zbdy20061001@163.com>
2024-01-02 16:42:52 +0800

    some updte

---
+++ java-samples/src/main/java/jmail/SendEmail.java
@@ -0,0 +1,65 @@
+package jmail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmail {
+	public static void main(String[] args) {
+		// Recipient's email ID needs to be mentioned.
+		String to = "632073163@qq.com";
+
+		// Sender's email ID needs to be mentioned
+		String from = "zbdy20061001@163.com";
+		final String username = "zbdy20061001";// change accordingly
+		final String password = "xxxxxxx";// change accordingly
+
+		// Assuming you are sending email through relay.jangosmtp.net
+		String host = "smtp.163.com";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "25");
+
+		// Get the Session object.
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+			// Create a default MimeMessage object.
+			Message message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+
+			// Set To: header field of the header.
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+			// Set Subject: header field
+			message.setSubject("Testing Subject");
+
+			// Now set the actual message
+			message.setText("Hello, this is sample for to check send " + "email using JavaMailAPI ");
+
+			// Send message
+			Transport.send(message);
+
+			System.out.println("Sent message successfully....");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
+//更多请阅读：https://www.yiibai.com/javamail_api/javamail_api_sending_simple_email.html
